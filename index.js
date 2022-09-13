1475bcbd2f5c09d5da1f265dae2b6331f05b0689
Nico Garbaccio <garbaccio20@gmail.com>
2022-09-13 09:57:21 -0400

    Render book on search

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const searchForm = document.querySelector('#search-form')
+
 function fetchFavorites(){
     fetch("")
     .then(data => data.json())
@@ -15,4 +17,35 @@ function deleteFavorite(id) {
         header: { "Content-Type": "application/json" },
     })
         .then(res => res.json())
+}
+
+// Search by author
+function authorSearch(first, last) {
+    searchForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        fetch(`https://openlibrary.org/search/authors.json?q=${first}%20${last}`)
+        .then(res => res.json())
+        .then(renderBook())
+    })
+}
+
+// Render search results
+function renderSearch() {
+    console.log("hey")
+}
+
+authorSearch()
+
+// Render book cards 
+function renderBook() {
+    const searchResults = document.querySelector('#search-results')
+    const resultsContainer = document.querySelector('.results-container')
+    const li = document.createElement('li')
+    const h3 = document.createElement('h3')
+    const img = document.createElement('img')
+    searchResults.style.display = "block"
+    resultsContainer.style.display = "block"
+    h3.textContent = "Test";
+    li.append(h3)
+    resultsContainer.append(li)
 }
