0a9d41c19b91d6ef057378d562bc625e9e864921
Nico Garbaccio <garbaccio20@gmail.com>
2022-09-13 18:15:54 -0400

    Add fave and queue buttons to cards

--- index.js
+++ index.js
@@ -52,11 +52,17 @@ function addSearch(data, name) {
         const li = document.createElement('li')
         const h3 = document.createElement('h3')
         const img = document.createElement('img')
+        const faveBtn = document.createElement('button')
+        faveBtn.className = "fave-button"
+        faveBtn.textContent = "Add to favorites"
+        const queueBtn = document.createElement('button')
+        queueBtn.className = 'queue-button'
+        queueBtn.textContent = "Add to queue"
         img.src = book.formats['image/jpeg']
         searchResults.style.display = "block"
         resultsContainer.style.display = "flex"
         h3.textContent = book.title
-        li.append(h3, img)
+        li.append(h3, img, faveBtn, queueBtn)
         resultsContainer.append(li)
     })
 }
@@ -69,13 +75,42 @@ function renderFeatured(data) {
         const li = document.createElement('li')
         const h3 = document.createElement('h3')
         const img = document.createElement('img')
+        const faveBtn = document.createElement('button')
+        faveBtn.className = "fave-button"
+        faveBtn.textContent = "Add to favorites"
+        const queueBtn = document.createElement('button')
+        queueBtn.className = 'queue-button'
+        queueBtn.textContent = "Add to queue"
         img.src = book.formats['image/jpeg']
         featuredBooks.style.display = "flex"
         h3.textContent = book.title
-        li.append(h3, img)
+        li.append(h3, img, faveBtn, queueBtn)
         featuredBooks.append(li)
     })
 }
+
+// function renderBooks() {
+//     const spliceBooks = data?.results.splice(0, 6)
+//     spliceBooks?.forEach(book => {
+//         const searchResults = document.querySelector('#search-results')
+//         const resultsContainer = document.querySelector('.results-container')
+//         const li = document.createElement('li')
+//         const h3 = document.createElement('h3')
+//         const img = document.createElement('img')
+//         const faveBtn = document.createElement('button')
+//         faveBtn.className = "fave-button"
+//         faveBtn.textContent = "Add to favorites"
+//         const queueBtn = document.createElement('button')
+//         queueBtn.className = 'queue-button'
+//         queueBtn.textContent = "Add to queue"
+//         img.src = book.formats['image/jpeg']
+//         searchResults.style.display = "block"
+//         resultsContainer.style.display = "flex"
+//         h3.textContent = book.title
+//         li.append(h3, img, faveBtn)
+//         featuredBooks.append(li)
+//     })
+// }
 
 // Invoking functions 
 authorSearch()
