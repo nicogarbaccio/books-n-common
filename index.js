3b2c508dd49626b9e0b422148c7a7dc140f59b3f
Kaeli Gilmore <mskaleemae2000@gmail.com>
2022-09-13 17:25:02 -0400

    login

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const searchForm = document.querySelector('.search-form')
 
+let loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () =>{
+  loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = () =>{
+  loginForm.classList.remove('active');
+}
+
 //Delete book cards from favorites
 function deleteFavorite(id) {
     fetch(`https://gutendex.com/books/${id}`, {
